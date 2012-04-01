d53e26e4a299954b9846b93c629c3bfc7033ee0b
senufo <fce.valeins@libertysurf.fr>
2012-04-01 19:17:23 +0200

    add epg description and list

--- default.py
+++ default.py
@@ -14,10 +14,11 @@ import channel
 import os, re
 import pickle
 import sys
+import time
 
 __author__     = "Senufo"
 __scriptid__   = "script.svdrpclient"
-__scriptname__ = "isvdrpclient"
+__scriptname__ = "svdrpclient"
 
 __addon__      = xbmcaddon.Addon(id=__scriptid__)
 
@@ -51,13 +52,12 @@ class VDRWindow(xbmcgui.WindowXML):
             ch = channel.Channel(message)
             self.channels.append(ch)
         self.vdrpclient.close() 
-        for ch in self.channels:
-            print "CHinit = %s " % ch
+##        for ch in self.channels:
+#            print "CHinit = %s " % ch
         
     def onInit( self ):
         if DEBUG == True: print "Branch Master"
         for ch in self.channels:
-            print "CH = %s " % ch
             listChannel = xbmcgui.ListItem(label=ch.name_tok)
             listChannel.setProperty( "description", 'desc' )
             listChannel.setProperty( "img" , 'img' )
@@ -66,7 +66,87 @@ class VDRWindow(xbmcgui.WindowXML):
  
             listChannel.setProperty("serveur", ch.name_tok )
             self.getControl( 1200 ).addItem( listChannel )
-   
+
+    def getEPG(self, ch):
+        #print 'CH EPG = %s ' % ch
+        Dialog = xbmcgui.DialogProgress()
+        #    locstr = __addon__.getLocalizedString(id=600) #Get News
+        Dialog.create('Please Wait')
+        #locstr = __addon__.getLocalizedString(id=601) #Please wait
+        Dialog.update(0, 'update')
+        #Variable pour la progression dans la boite de dialogue²
+        up = 1
+
+        for c_name in self.channels:
+            #print "CHinit = %s " % ch
+            if c_name.name_tok == ch:
+                print "No = %s, c_name.name_tok = %s" % (c_name.no,c_name.name_tok)
+                c_no = c_name.no
+        self.vdrpclient = svdrp.SVDRPClient(VDR_HOST, VDR_PORT)
+        self.vdrpclient.send_command('lste %s' % c_no)
+        Titre, SousTitre, Description = ('','','')
+        ev = event.Event()
+        #nbEPG = len(self.vdrpclient.read_response())
+        NbEPG = 100
+        for num, flag, message in self.vdrpclient.read_response():
+            print "MESSAGE = %s " % message
+            if message[0] == 'T':
+                ev.title = message[2:]
+            elif message[0] == 'S':
+                ev.subtitle= message[2:]
+            elif message[0] == 'D':
+                Description  = message[2:]
+                ev.desc = Description.replace('|','\n')
+            elif message[0] == 'E' and message != 'End of EPG data':
+                # event start
+                ev.parseheader(message[2:])
+                ev.source = 'vdr'
+                print "Start = %s, durée = %s, id = %s" % (ev.start,ev.dur,ev.id)
+                #START = Wed Apr 11 01:10:00 2012
+                #time.struct_time(tm_year=2012, tm_mon=4, tm_mday=10, tm_hour=23, tm_min=10, tm_sec=0, tm_wday=1, tm_yday=101, tm_isdst=0) 
+                print 'getitme = %s ' % time.gmtime(int(ev.start))
+
+            if message[0] == 'e':
+                #(year,mois, mday, heure, min, sec) = time.gmtime(int(ev.start))
+                time_start = time.gmtime(int(ev.start))
+                stop = ev.start + ev.dur
+                time_stop = time.gmtime(int(stop))
+                #print "Start = %s, durée = %s, id = %s" % (ev.start,ev.dur,ev.id)a
+                print ('%02d:%02d - %02d:%02di => %s' %
+                    (time_start.tm_hour,time_start.tm_min,time_stop.tm_hour,time_stop.tm_min,
+                    ev.title))
+                epg_data = ('%02d:%02d - %02d:%02d : %s' %
+                    (time_start.tm_hour,time_start.tm_min,time_stop.tm_hour,time_stop.tm_min,
+                    ev.title))
+
+                listEPGItem = xbmcgui.ListItem( label=epg_data)
+                listEPGItem.setProperty( "description", ev.desc )
+                listEPGItem.setProperty( "date", time.strftime('%A, %d/%m/%Y',time_start))
+                self.getControl( 120 ).addItem( listEPGItem )
+                Titre, SousTitre, Description = ('','','')
+            up2 = int((up*100)/NbEPG)
+            #print "UP = %d " % up
+            up += 1
+            Dialog.update(up2, 'Get EPG', 'Please wait')
+        Dialog.close()       
+
+        self.vdrpclient.close()
+
+ 
+    def onClick( self, controlId ):
+        #print "onClick controId = %d " % controlId
+        if (controlId == 1200):
+            label = self.getControl( controlId
+                                   ).getSelectedItem().getProperty('serveur')
+            self.getEPG(label)
+        elif (controlId == VIDEO):
+            label = self.getControl( FEEDS_LIST
+                                   ).getSelectedItem().getProperty('video')
+
+        elif (controlId == QUIT):
+            self.close()
+
+  
 mydisplay = VDRWindow( "script-svdrp-main.xml" , __cwd__, "Default")
 mydisplay.doModal()
 del mydisplay
--- test_svdrp.py
+++ test_svdrp.py
@@ -5,6 +5,7 @@
 # Distribution allowed under the Simplified BSD License.
 import telnetlib
 import event
+import time
 from datetime import datetime, timedelta
 import re
 import sys
@@ -52,7 +53,6 @@ client = SVDRPClient(VDR_HOST, VDR_PORT)
 #num, message = client.confirm_shutdown()
 #client.send_command('chan')
 #client.send_command('help')
-#client.send_command('lste 1')
 client.send_command('lstc')
 #num = 0
 #message = client.read_line()
@@ -96,6 +96,8 @@ for num, flag, message in client.read_response():
         name,nametokens,name_tok,freq,pol,source,srate,vpid,apids,tpid,ca)
     channels.append(name)
 
+client.send_command('lste 1')
+
 for channel in channels:
     print channel
 Titre, SousTitre, Description = ('','','')
@@ -114,12 +116,24 @@ for num, flag, message in client.read_response():
         ev.parseheader(message[2:])
         ev.source = 'vdr'
         print "Start = %s, durée = %s, id = %s" % (ev.start,ev.dur,ev.id)
+        #START = Wed Apr 11 01:10:00 2012
+        print 'START = %s ' % time.ctime(int(ev.start))
+        #time.struct_time(tm_year=2012, tm_mon=4, tm_mday=10, tm_hour=23, tm_min=10, tm_sec=0, tm_wday=1, tm_yday=101, tm_isdst=0) 
+        print 'getitme = %s ' % time.gmtime(int(ev.start))
         #ev.channel = ch
 
     if message[0] == 'e':
-        print '\nTitre = %s\nSousTitre = %s\n Desc = %s ' % (
-                                        ev.title,ev.subtitle,ev.desc)
-        print "Start = %s, durée = %s, id = %s" % (ev.start,ev.dur,ev.id)
+        #(year,mois, mday, heure, min, sec) = time.gmtime(int(ev.start))
+        time_start = time.gmtime(int(ev.start))
+        stop = ev.start + ev.dur
+        time_stop = time.gmtime(int(stop))
+        #print '%02d:%02d - %02d:%02d' % (time_start.tm_hour,time_start.tm_min,time_stop.tm_hour,time_stop.tm_min)
+        #print '\nTitre = %s\nSousTitre = %s\n Desc = %s ' % (
+        #                                ev.title,ev.subtitle,ev.desc)
+        #print "Start = %s, durée = %s, id = %s" % (ev.start,ev.dur,ev.id)a
+        print ('%02d:%02d - %02d:%02di => %s' %
+            (time_start.tm_hour,time_start.tm_min,time_stop.tm_hour,time_stop.tm_min,
+            ev.title))
         Titre, SousTitre, Description = ('','','')
 client.close()
 
